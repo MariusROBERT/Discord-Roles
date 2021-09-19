54b38f3a465dc1f2e649a69297bf8977def85a06
Marius ROBERT <mariusrobert380@gmail.com>
2021-09-19 20:10:13 +0200

    1st push

---
+++ main.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import tasks, commands
+import json
+
+fileJson = "token.json"
+with open(fileJson, "r") as f:
+    infos = json.load(f)
+token = infos["tokenDiscord"]
+id_server = infos["idServeur"]
+liste_roles = infos["roles"]
+blabla = """Bienvenue sur le serveur discord du DUT Informatique
+Afin d'obtenir vos rôles, merci de m'indiquer dans quelle classe vous êtes"""
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_member_join(member):
+    await client.send_message(member, blabla)
+
+@client.event
+async def on_message(message):
+    if message.author.bot == True:
+        return
+
+    if type(message.channel) == discord.channel.DMChannel:
+        texte = message.content
+        user = message.channel.recipient
+        print("{} from {}".format(texte, user))
+
+        for i in liste_roles:
+            if i in texte:
+                #add_role(message.author)
+                serveur = client.get_guild(id_server)
+                #print(serveur.name)
+                #print(user.mention[2:-1])
+                #print(user.roles)
+                #print(serveur.members)
+                member = serveur.get_member(int(user.mention[2:-1]))
+                print(member.joined_at)
+                #await edit(roles= )
+                role = discord.utils.get(serveur.roles, name=i)
+                await member.add_roles(role, reason="Seig Role Bot")
+                await message.author.send("Un rôle vous a été assigné")
+                return
